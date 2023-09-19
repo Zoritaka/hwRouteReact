911001225c13d611f1619809c68cb6d27b5d776c
диханбаевал <Dikhanbaev.2018@mail.ru>
2023-09-19 16:41:44 +0600

    3 step commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+
+import Attractions1 from './pages/attractions1';
+import Attractions2 from './pages/attractions2';
+import Attractions3 from './pages/attractions3';
+import City from './pages/City';
+import Info from './pages/info';
+import Photo from './pages/photo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<City/>}></Route>
+        <Route path="/photo" element={<Photo/>}></Route>
+        <Route path="/info" element={<Info/>}></Route>
+        <Route path="/attractions1" element={<Attractions1/>}></Route>
+        <Route path="/attractions2" element={<Attractions2/>}></Route>
+        <Route path="/attractions3" element={<Attractions3/>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
